3a3f625df8fec6780299f91243c1f2afecb40a4b
PamelaMoreno <parker.moreno15@gmail.com>
2022-02-22 07:08:23 -0600

    Sitio personal

---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title> Pamela Stephanie Moreno Parker</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body id="page-top">
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top text-uppercase" id="mainNav" style="background: rgb(85,45,8);">
+        <div class="container"><a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-size: 22px;">Pamela Stephanie Moreno Parker</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler text-white navbar-toggler-right text-uppercase rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation" style="background: rgb(227,166,122);"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link text-white py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#portfolio" style="background: rgb(227,166,122);">Acerca de</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link text-white py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about" style="background: rgb(183,120,74);">PORTAFOLIO</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link text-white py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#contact" style="background: rgb(128,76,39);">Contacto</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="text-center text-white masthead" style="background: rgb(227,166,122);">
+        <div class="container">
+            <h2 class="font-weight-light mb-0" style="color: rgb(0,0,0);">Estudiante de informática en Universidad Nacional Autónoma de México&nbsp;</h2>
+        </div>
+    </header>
+    <section class="text-dark portfolio" id="portfolio" style="color: var(--dark);background: rgb(183,120,74);">
+        <div class="container">
+            <h2 class="text-uppercase text-center text-white"><br>Acerca de</h2>
+            <h2 class="text-uppercase text-center text-secondary">&nbsp; &nbsp;</h2>
+            <h2 class="text-uppercase text-center text-secondary"></h2>
+            <section class="article-list">
+                <div class="container">
+                    <div class="intro"></div>
+                    <div class="row articles">
+                        <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/krakenimages-376KN_ISplE-unsplash.jpg"></a>
+                            <h3 class="name">Habilidades</h3>
+                            <p class="description">Capacidad para proponer nuevas ideas</p>
+                            <p class="description">Capacidad para identificar oportunidades de mejora</p>
+                            <p class="description">Capacidad para colaborar con personas</p><a class="action" href="#"></a>
+                        </div>
+                        <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/nordwood-themes-yyMJNPgQ-X8-unsplash.jpg"></a>
+                            <h3 class="name">Aptitudes</h3>
+                            <p class="description">Desarrollo de programas en lenguaje c y java</p>
+                            <p class="description">Conocimiento en diseño de algoritmos</p>
+                            <p class="description">Desarrollo de aplicaciones móviles en Android Studio</p><a class="action" href="#"></a>
+                        </div>
+                        <div class="col-sm-6 col-md-4 item"><a href="#"><img class="img-fluid" src="assets/img/j-kelly-brito-PeUJyoylfe4-unsplash.jpg"></a>
+                            <h3 class="name">Idioma</h3>
+                            <p class="description">Inglés:</p>
+                            <p class="description">Escrito (Básico)</p>
+                            <p class="description">Hablar (Básico)</p>
+                            <p class="description">Lectura (Básico)</p><a class="action" href="#"></a>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </section>
+    <section class="text-white mb-0" id="about" style="background: rgb(128,76,39);">
+        <div class="container">
+            <h2 class="text-uppercase text-center text-white">Portafolio</h2>
+            <h2 class="text-uppercase text-center text-white">&nbsp; &nbsp;&nbsp;</h2>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <section class="features-boxed" style="background: #ffffff;">
+                        <div class="container">
+                            <div class="intro"></div>
+                            <div class="row justify-content-center features">
+                                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                                    <div class="box"><i class="fa fa-file-o icon"></i>
+                                        <h3 class="name">Desarrollo de trabajos escritos</h3>
+                                        <p class="description">He desarrollado diversos trabajos escolares acerca de como la informática influye en nuestra vida cotidiana.</p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                                    <div class="box"><i class="fa fa-laptop icon"></i>
+                                        <h3 class="name">Desarrollo de aplicaciones de inventarios</h3>
+                                        <p class="description">He desarrollado con lenguajes como C y java diversos programas relacionados con el inventario de las empresas.</p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                                    <div class="box"><i class="fa fa-tablet icon"></i>
+                                        <h3 class="name">Desarrollo de aplicaciones móviles</h3>
+                                        <p class="description">He desarrollado un proyecto de aplicaciones móviles con Android Studio para el registro de actividades culturales.</p>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 col-md-5 col-lg-4 item">
+                                    <div class="box"><i class="fa fa-list-alt icon"></i>
+                                        <h3 class="name">Desarrollo de proyecto de telecomunicaciones.</h3>
+                                        <p class="description"></p>
+                                        <p class="description">He desarrollado diversas practicas y un proyecto de diversos temas de telecomunicaciones, principalmente sobre la conectividad de ipv4 e ipv6.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </s
+
+ection>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="text-center footer" style="background: rgb(85,45,8);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mb-5 mb-lg-0">
+                    <h4 class="text-uppercase mb-4">localización</h4>
+                    <p>Coyoacán, cdmx</p>
+                </div>
+                <div class="col-md-4 mb-5 mb-lg-0">
+                    <h4
+
+ class="text-uppercase" id="contact">CONTACTO</h4>
+                    <ul class="list-inline">
+                        <li class="list-inline-item"><a class="btn btn-outline-light text-center btn-social rounded-circle" role="button" href="https://github.com/PamelaMoreno/PamelaMoreno.git"><i class="fa fa-github fa-fw"></i></a></li>
+                        <li class="list-inline-item"></li>
+                        <li class="list-inline-item"></li>
+                        <li class="list-inline-item"><a class="btn btn-outline-light text-center btn-social rounded-circle" role="button" href="https://www.linkedin.com/in/pamela-stephanie-moreno-parker-3497bb194"><i class="fa fa-linkedin fa-fw"></i></a></li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <h4 class="text-uppercase mb-4">About Freelancer</h4>
+                    <p class="lead mb-0"><span>Freelance is a free to use, open source Bootstrap theme.&nbsp;</span></p>
+                </div>
+            </div>
+        </div>
+
+
+    </footer>
+    <div class="text-center text-white copyright py-4" style="background: rgb(40,19,0);">
+        <div class="container"><small>Copyright ©&nbsp;Pamela Stephanie Moreno Parker 2021</small></div>
+    </div>
+    <div class="d-lg-none scroll-to-top position-fixed rounded"><a class="text-center text-white d-block js-scroll-trigger rounded" href="#page-top"><i class="fa fa-chevron-up"></i></a></div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-1">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                       
+
+     <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/cabin.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-2">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                       
+
+     <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/cake.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-3">
+
+
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/circus.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-4">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/game.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-5">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/safe.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="modal text-center" role="dialog" tabindex="-1" id="portfolio-modal-6">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button></div>
+                <div class="modal-body">
+                    <div class="container text-center">
+                        <div class="row">
+                            <div class="col-lg-8 mx-auto">
+                                <h2 class="text-uppercase text-secondary mb-0">Project Name</h2>
+                                <hr class="star-dark mb-5"><img class="img-fluid mb-5" src="assets/img/portfolio/submarine.png">
+                                <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                            </div>
+                        </div>
+                    </div>
+               
+ </div>
+                <div class="modal-footer pb-5"><a class="btn btn-primary btn-lg mx-auto rounded-pill portfolio-modal-dismiss" role="button" data-dismiss="modal"><i class="fa fa-close"></i>&nbsp;Close Project</a></div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
+
+
